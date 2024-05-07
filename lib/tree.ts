fd9c8e5fd71595281421fc7675dc217b31014930
Jim Fisher <jameshfisher@gmail.com>
2024-05-07 10:27:24 +0100

    ignore some paths when gathering course files

--- lib/tree.ts
+++ lib/tree.ts
@@ -1,9 +1,18 @@
 import * as fs from 'fs/promises';
 import * as path from 'path';
 
+// TODO make this ignore list configurable, e.g. just use .gitignore
+const ignore = ['.git', 'venv'];
+
 export async function* tree(root: string): AsyncGenerator<string> {
+  if (ignore.includes(path.basename(root))) return;
+
   const stats = await fs.stat(root);
-  if (stats.isDirectory())
-    for (const c of await fs.readdir(root)) yield* tree(path.join(root, c));
-  else yield root;
+  if (stats.isDirectory()) {
+    for (const c of await fs.readdir(root)) {
+      yield* tree(path.join(root, c));
+    }
+  } else {
+    yield root;
+  }
 }
