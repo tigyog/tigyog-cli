6421b8c48d35503857d93166f336fbf713c4bfb7
Jim Fisher <jameshfisher@gmail.com>
2024-05-22 11:00:21 +0100

    matching vs code style

--- lib/stringifyMarkdown.ts
+++ lib/stringifyMarkdown.ts
@@ -7,7 +7,12 @@ import { unified } from 'unified';
 
 export function stringifyMarkdownRoot(root: Root): string {
   const processor = unified()
-    .use(remarkStringify, { listItemIndent: 'one' })
+    .use(remarkStringify, {
+      listItemIndent: 'one',
+      bullet: '-', // to match VS Code's default Markdown formatter (Shift+Alt+F)
+      emphasis: '_', // to match VS Code's default Markdown formatter (Shift+Alt+F)
+      fences: true, // to match VS Code's default Markdown formatter (Shift+Alt+F)
+    })
     .use(remarkFrontmatter, ['yaml'])
     .use(remarkMath)
     .use(remarkDirective);
