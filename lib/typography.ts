c7379bea7dce33e805b2ed84bd8cf2b6c9b4c588
Jim Fisher <jameshfisher@gmail.com>
2024-08-27 11:10:20 +0100

    fix quotes

--- lib/typography.ts
+++ lib/typography.ts
@@ -1,6 +1,7 @@
 // TODO single quotes, dashes
+
 export const smartQuotes = (text: string): string => {
-  text = text.replace(/"(?=\w|$)/g, '“');
-  text = text.replace(/(?<=\w|^)"/g, '”');
+  text = text.replace(/(\W|^)"(\w)/g, '$1“$2');
+  text = text.replace(/(\w|[.,!?])"(\W|$)/g, '$1”$2');
   return text;
 };
