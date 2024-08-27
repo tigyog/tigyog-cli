be1b988435316f19a3ecf1d811cf2012a36b2d7b
Jim Fisher <jameshfisher@gmail.com>
2024-08-27 12:37:16 +0100

    single quotes and apostrophes

--- lib/typography.test.ts
+++ lib/typography.test.ts
@@ -2,9 +2,9 @@ import { describe, expect, it } from 'vitest';
 import { smartQuotes } from './typography';
 
 describe('smartQuotes', () => {
-  it('should replace straight double quotes with curly quotes', () => {
-    expect(smartQuotes('"Hello, world!", he said. "What\'s up?"')).toBe(
-      "“Hello, world!”, he said. “What's up?”",
-    );
+  it('should replace straight quotes with curly quotes', () => {
+    const i = `"Hello, world," he said. 'It's a joy to be alive!'`;
+    const o = `“Hello, world,” he said. ‘It’s a joy to be alive!’`;
+    expect(smartQuotes(i)).toBe(o);
   });
 });
--- lib/typography.ts
+++ lib/typography.ts
@@ -1,7 +1,16 @@
-// TODO single quotes, dashes
+// TODO dashes
 
 export const smartQuotes = (text: string): string => {
+  // Double quotes
   text = text.replace(/(\W|^)"(\w)/g, '$1“$2');
   text = text.replace(/(\w|[.,!?])"(\W|$)/g, '$1”$2');
+
+  // Single quotes
+  text = text.replace(/(\W|^)'(\w)/g, '$1‘$2');
+  text = text.replace(/(\w|[.,!?])'(\W|$)/g, '$1’$2');
+
+  // Apostrophe
+  text = text.replace(/(\w)'(\w)/g, '$1’$2');
+
   return text;
 };
